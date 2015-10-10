7b0da1dc0d400f4ec3b9907598a31473de915bc6
Christy Haragan <christy@christy.haragan@gmail.com>
2015-10-10 12:47:13 +0100

    Use excludes in tsconfig

--- lib/readerWriter.d.ts
+++
@@ -1,4 +0,0 @@
-import * as s from 'typescript-schema';
-import * as ts from 'typescript';
-export declare function writePackageSchema(pkgDir: string, options?: ts.CompilerOptions, host?: ts.CompilerHost, schemaPath?: string): s.KeyValue<s.reflective.Module>;
-export declare function readPackageSchema(pkgDir: string, schemaPath?: string): s.KeyValue<s.reflective.Module>;
